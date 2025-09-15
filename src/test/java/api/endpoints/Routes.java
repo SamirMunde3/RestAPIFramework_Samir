ac5c29c63c11f5b3d23a7c406e1464d4ff49b5f8
samirmunde30 <samirmunde3007@gmail.com>
2025-09-15 16:05:39 +0530

    username change

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -8,7 +8,7 @@ Update User	PUT	https://petstore.swagger.io/v2/user/{username}
 Delete User	DELETE	https://petstore.swagger.io/v2/user/{username}
  */
 	
-	//samir munde git push check
+	
 	public static String base_url = "https://petstore.swagger.io/v2";
 	
 	//user module urls
